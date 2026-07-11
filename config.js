b4b90b0ea0e7683baf47404dd05ddf51b7babe0d
Shavkat <Shavkatjon2@gmail.com>
2026-07-11 12:33:01 +0500

    Harden mobile receipt wrapping

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.VIDIPAY_API_BASE = "https://vidipay-backend-1.onrender.com";
-window.VIDIPAY_FRONTEND_BUILD = "frontend-admin-readiness-20260711-v7";
+window.VIDIPAY_FRONTEND_BUILD = "frontend-admin-readiness-20260711-v8";
