414d0154cc97e76facefcdf11291403261f0aaca
shshavkatjon2-blip <shshavkatjon2@gmail.com>
2026-07-26 23:40:03 +0500

    Add idempotent payment requests and frontend fixes

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.VIDIPAY_API_BASE = "https://vidipay-backend-1.onrender.com";
-window.VIDIPAY_FRONTEND_BUILD = "frontend-backend-resilience-20260724-v24";
+window.VIDIPAY_FRONTEND_BUILD = "frontend-edge-transaction-security-20260726";
